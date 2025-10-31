1cb2d03adf6d06b1b5a6d42f52a66c76f9f1f4dd
datazarpy <antonvichon@gmail.com>
2025-10-31 13:12:00 +0200

    Update Python OOP Shapes.py

--- Python OOP Shapes.py
+++ Python OOP Shapes.py
@@ -32,31 +32,7 @@ class Triangle(Shape):
             print(spaces + stars)
 
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def draw(self):
-        if self.radius <= 0:
-            print("Radius must be a positive integer.")
-            return
-
-        diameter = 2 * self.radius
-
-        for y in range(diameter + 1):
-            for x in range(diameter + 1):
-                distancex = x - self.radius
-                distancey = y - self.radius
-                distance = (distancex**2 + distancey**2) ** 0.5
-
-                if distance <= self.radius:
-                    print("*", end=" ")
-                else:
-                    print(" ", end="")
-            print()
-
-
-class Rhombus(Shape):
+class Parallelogram(Shape):
     def __init__(self, size):
         self.size = size
 
@@ -64,18 +40,9 @@ class Rhombus(Shape):
         if self.size <= 0:
             print("Size must be a positive integer.")
             return
-        for i in range(self.size):
-            spaces_in = " " * (self.size - i - 1)
-            stars = "*" * (2 * i - 1)
-            if i == 0:
-                print(spaces_in + "*")
-            else:
-                print(spaces_in + stars + "*")
 
-        for i in range(self.size - 1, 0, -1):
-            stars = "*" * (2 * i - 1)
-            spaces = " " * (self.size - i)
-            print(spaces + stars)
+        for i in range(self.size, 0, -1):
+            print((" " * i) + "*" * 10)
 
 
 if __name__ == "__main__":
@@ -83,14 +50,10 @@ if __name__ == "__main__":
     rect.draw()
     print()
 
-    tri = Triangle(8)
+    tri = Triangle(5)
     tri.draw()
     print()
 
-    rhom = Rhombus(8)
-    rhom.draw()
+    para = Parallelogram(5)
+    para.draw()
     print()
-
-    # circ = Circle(10)
-    # circ.draw()
-    # print()
